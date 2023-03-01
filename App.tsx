5d9c1d7354c3cc6c0b17b06ad0a97dc1dc91b48f
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-03-02 00:32:29 +1300

    Forms with react-hook-form
    Compare to the previous commit:
    
    da58cee9aa148e3e38669f4b8128d36ec79f003b

--- App.tsx
+++ App.tsx
@@ -1,136 +1,102 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { View, Text, TextInput, Button, Alert } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 import { styles } from "./styles";
 
+//* Define Form data type
+type FormData = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
 export default function App() {
-  //* Form values
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  //* Form Error Values
-  const [usernameError, setUsernameError] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+  //* User useForm Hook from react-hook-form
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm<FormData>();
 
-  //* Username Error Value handling
-  useEffect(() => {
-    handelUsernameError();
-  }, [username]);
+  console.log("데이터가 변할때마다 실시간으로 관찰 가능함\n", watch());
 
-  useEffect(() => {
-    handelEmailError();
-  }, [email]);
-
-  useEffect(() => {
-    handelPasswordError();
-  }, [password]);
-
-  //* Email Error Value handling
-  const handelUsernameError = () => {
-    // Empty username
-    if (!username) {
-      setUsernameError("Username is required");
-      return;
-    }
-
-    // Valid case -> No Error Message
-    setUsernameError("");
-  };
-
-  //* Email Error Value handling
-  const handelEmailError = () => {
-    // Empty email
-    if (!email) {
-      setEmailError("Email is required");
-      return;
-    }
-
-    // Not valid email
-    if (!isValidEmail(email)) {
-      setEmailError("Email is not vaild!");
-      return;
-    }
-
-    // Valid case -> No Error Message
-    setEmailError("");
-  };
-
-  //* Password Error Value handling
-  const handelPasswordError = () => {
-    // Empty password
-    if (!password) {
-      setPasswordError("Password is required");
-      return;
-    }
-
-    // Not valid password
-    if (!isValidPassword(password)) {
-      setPasswordError("Password is too short!");
-      return;
-    }
-
-    // Valid case -> No Error Message
-    setPasswordError("");
-  };
-
-  //* Email validator
-  const isValidEmail = (email: string) => {
-    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    return emailRegex.test(email);
-  };
-
-  //* Password validator
-  const isValidPassword = (password: string) => {
-    return password.trim().length >= 6;
-  };
-
-  //* Submit Button handling
-  const handleSubmit = () => {
-    // Exceptions handling
-    if (usernameError) return;
-    if (emailError) return;
-    if (passwordError) return;
-
-    // All good
+  const onSubmit = (data: FormData) => {
     Alert.alert("Forms are submitted!");
-
-    // reset
-    setUsername("");
-    setEmail("");
-    setPassword("");
+    reset();
   };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={username}
-        onChangeText={setUsername}
-        placeholder="Username"
-        autoCapitalize="none"
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            style={styles.input}
+            value={value}
+            onChangeText={onChange}
+            placeholder="Username"
+            autoCapitalize="none"
+          />
+        )}
+        name="username"
+        rules={{ required: "Username is required" }}
+        defaultValue=""
       />
-      {usernameError && <Text style={styles.error}>{usernameError}</Text>}
+      {errors.username && (
+        <Text style={styles.error}>{errors.username.message}</Text>
+      )}
 
-      <TextInput
-        style={styles.input}
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Email"
-        keyboardType="email-address"
-        autoCapitalize="none"
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            style={styles.input}
+            value={value}
+            onChangeText={onChange}
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        )}
+        name="email"
+        rules={{
+          required: "Email is required",
+          pattern: {
+            value: emailRegex,
+            message: "Email is not valid",
+          },
+        }}
+        defaultValue=""
       />
-      {emailError && <Text style={styles.error}>{emailError}</Text>}
+      {errors.email && <Text style={styles.error}>{errors.email.message}</Text>}
 
-      <TextInput
-        style={styles.input}
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Password"
-        secureTextEntry
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            style={styles.input}
+            value={value}
+            onChangeText={onChange}
+            placeholder="Password"
+            secureTextEntry
+          />
+        )}
+        name="password"
+        rules={{
+          required: "Password is required",
+          minLength: { value: 6, message: "Password is too short" },
+        }}
+        defaultValue=""
       />
-      {passwordError && <Text style={styles.error}>{passwordError}</Text>}
+      {errors.password && (
+        <Text style={styles.error}>{errors.password.message}</Text>
+      )}
 
-      <Button title="Submit" onPress={handleSubmit} />
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 }
